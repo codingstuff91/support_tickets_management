944a50e9c3dce02e9558cb6790b5c0dcc62868ba
Matt MARTIN <codingstuff91@gmail.com>
2022-11-27 15:33:10 +0100

    Create Ticket model, migration, factory and relationships

---
+++ app/Models/Ticket.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Ticket extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description', 
+        'priority',
+        'status',
+        'user_id',
+        'agent_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function agent()
+    {
+        return $this->belongsTo(User::class, 'agent_id');
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function labels()
+    {
+        return $this->belongsToMany(Label::class);
+    }
+}
---
+++ database/migrations/2022_11_27_141339_create_tickets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            $table->text('description');
+            $table->string('priority');
+            $table->string('status');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('agent_id')->nullable();
+            $table->foreign('agent_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
