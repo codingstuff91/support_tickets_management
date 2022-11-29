48ac2a73e492128645c6be3f6b808ec72cb48f52
Matt MARTIN <codingstuff91@gmail.com>
2022-11-29 20:36:50 +0100

    Edit User and Tickets migrations

--- database/migrations/2022_11_27_145200_create_users_table.php
+++ database/migrations/2022_11_27_145200_create_users_table.php
@@ -15,8 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('role_id');
-            $table->foreign('role_id')->references('id')->on('roles');
+            $table->unsignedBigInteger('role_id')->default(1);
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
