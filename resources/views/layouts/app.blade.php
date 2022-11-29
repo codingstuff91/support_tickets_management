fc49b1906b38f9e1a25b46486d4168d2cf7161ee
Matt MARTIN <codingstuff91@gmail.com>
2022-11-29 20:38:20 +0100

    Edit App Layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,9 +11,6 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
@@ -31,5 +28,6 @@
                 {{ $slot }}
             </main>
         </div>
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
