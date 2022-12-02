50dfa434b9c2abf41b6b1dd0e89be52c3cf74591
Matthieu MARTIN <codingstuff91@gmail.com>
2022-12-02 18:30:06 +0100

    Delete userSeeder and create Admin, Agent and regular users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
+use App\Models\User;
 use App\Models\Label;
 use App\Models\Ticket;
 use App\Models\Category;
@@ -20,8 +22,16 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             CategorySeeder::class,
             LabelSeeder::class,
-            UserSeeder::class,
         ]);
+
+        // Create 1 admin user
+        User::factory()->create(['role_id' => Role::whereName('admin')->get()->first()->id]);
+
+        // Create 1 agent user
+        User::factory()->create(['role_id' => Role::whereName('agent')->get()->first()->id]);
+        
+        // Create 5 regular users
+        User::factory(5)->create();
 
         // Create a ticket with one label and one category
         $ticket = Ticket::factory()->create();
