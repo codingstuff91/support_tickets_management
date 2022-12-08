7f834cb675b9918f5de97cbc9e34034ccfe30b5d
Matthieu MARTIN <codingstuff91@gmail.com>
2022-12-08 10:42:58 +0100

    Add button to create a ticket

--- resources/views/ticket/index.blade.php
+++ resources/views/ticket/index.blade.php
@@ -1,8 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Tickets') }}
+            {{ __('Support Tickets') }}
         </h2>
+        <div>
+            <button class="bg-green-500 py-2 px-4 rounded-lg text-white">
+                <a href="{{ route('tickets.create') }}">Create a ticket</a>
+            </button>
+        </div>
     </x-slot>
 
     <div class="py-4">
