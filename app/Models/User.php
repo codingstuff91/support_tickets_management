e409721f17bc9d197dbfef7c07f403e70e79fb49
Matthieu MARTIN <codingstuff91@gmail.com>
2022-12-09 11:32:53 +0100

    Fix User and Role relation error

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,7 +47,7 @@ class User extends Authenticatable
 
     public function role()
     {
-        return $this->hasOne(Role::class);
+        return $this->belongsTo(Role::class);
     }
 
     public function comments()
